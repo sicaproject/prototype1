e6b64e12d849ccd86f8099005880d05c2a2921f7
Mudit Bagora <muditbagora@gmail.com>
2020-09-20 15:49:39 +0530

    added pw change

--- dash/urls.py
+++ dash/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("user1/", views.user1, name = 'user1'),
     path("qna/", views.qna, name = 'qna'),
     path("classs/", views.classs, name = 'classs'),
-    path("profile/",views.profile,name = 'profile')
+    path("profile/",views.profile,name = 'profile'),
+    path("changePW/",views.changePW,name = 'changePW')
 ]
--- dash/views.py
+++ dash/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
 
 # Create your views here.
 def user1(request):
@@ -24,5 +26,23 @@ def classs(request):
 def profile(request):
     if request.user.is_authenticated:
         return render(request,'dash/profile.html')
+    else:
+        return redirect('/account/login')
+
+def changePW(request):
+    if request.user.is_authenticated:
+        pw = request.POST.get('pw',False)
+        npw = request.POST.get('password1',False)
+        if request.user.check_password(pw):
+            u = request.user
+            u.set_password(npw)
+            u.save()
+            messages.info(request,'Password changed successfully ;)')
+            return redirect('/account/login')
+        else:
+            wp = False
+            if request.method == "POST":
+                wp = True
+            return render(request,'dash/changePW.html',{'wp':wp})
     else:
         return redirect('/account/login')
